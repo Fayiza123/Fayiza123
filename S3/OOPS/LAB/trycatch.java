f694e56e9f90da8c57946a10a99584d394d9c31a
Fayiza123 <fayizakh133@gmail.com>
2024-11-05 09:19:34 +0530

    Add files via upload

---
+++ S3/OOPS/LAB/trycatch.java
@@ -0,0 +1,25 @@
+//Program to show the usage of try, catch, finally, throws
+//Fayiza_28
+import java.io.*;
+class main
+{
+	public static void main(String args[])
+	{
+		try
+		{
+			int []a={1,2,3,4,5};
+			a[10]=10;
+		}
+		catch(Exception e)
+		{
+			System.out.println("Arrayoutofindex exception");
+		}
+	finally
+	{
+		System.out.println("finally block executed successfully");
+	}
+}
+}
+
+                  
+                                    
