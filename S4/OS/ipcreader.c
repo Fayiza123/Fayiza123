75c20d557ed11b95d03acdc75026abcb7e00b41d
Fayiza K H <fayizakh133@gmail.com>
2025-03-10 11:31:39 +0530

    Add files via upload

---
+++ S4/OS/ipcreader.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/shm.h>
+#include<sys/ipc.h>
+#include<string.h>
+#include<unistd.h>
+void main()
+{
+     int id;
+     void *sm;
+     char buf[100];
+     id=shmget((key_t)1222,1024,0666);
+     printf("Key of shared memory is %d\n",id);
+     sm=shmat(id,NULL,0);
+     printf("Process attached at %p\n",sm);
+     printf("Data read from memory:\n%s",(char*)sm);
+     
+     strcpy(buf,sm);
+     int a=buf[0]-'0';
+     int b=buf[2]-'0';
+     
+     printf("Sum is %d\n",a+b);
+}      
